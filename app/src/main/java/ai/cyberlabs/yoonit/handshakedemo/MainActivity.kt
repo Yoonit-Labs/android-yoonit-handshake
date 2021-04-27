ea45d5eca379478150e7f47c9f798867ee3f8729
Goulartvic <goulart.vic@gmail.com>
2021-04-27 16:47:47 -0300

    refactor: remove HandshakeListener refactor: replace HandshakeListener to closure

--- app/src/main/java/ai/cyberlabs/yoonit/handshakedemo/MainActivity.kt
+++ app/src/main/java/ai/cyberlabs/yoonit/handshakedemo/MainActivity.kt
@@ -1,33 +1,23 @@
 package ai.cyberlabs.yoonit.handshakedemo
 
 import ai.cyberlabs.yoonit.yoonit.handshake.Handshake
-import ai.cyberlabs.yoonit.yoonit.handshake.HandshakeListener
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val handshakeListener: HandshakeListener = object : HandshakeListener {
-        override fun continueExecution() {
-            Toast.makeText(applicationContext, "SUCCESS", Toast.LENGTH_LONG).show()
-        }
-
-        override fun handleFailedUpdate(type: String, isPerformingUpdate: Boolean, result: String) {
-            Toast.makeText(applicationContext, result, Toast.LENGTH_LONG).show()
-        }
-
-    }
-
     private lateinit var handshake: Handshake
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        this.handshake = Handshake(applicationContext, handshakeListener)
+        this.handshake = Handshake(applicationContext)
 
-        update_fingerprint.setOnClickListener { handshake.updateFingerprint(BuildConfig.PUBLIC_KEY, BuildConfig.URL,) }
+        update_fingerprint.setOnClickListener {
+            handshake.updateFingerprint(BuildConfig.PUBLIC_KEY, BuildConfig.URL)
+            { Toast.makeText(applicationContext, it, Toast.LENGTH_LONG).show() }
+        }
     }
 }
--- yoonit-handshake/src/main/java/ai/cyberlabs/yoonit/yoonit/handshake/Handshake.kt
+++ yoonit-handshake/src/main/java/ai/cyberlabs/yoonit/yoonit/handshake/Handshake.kt
@@ -2,20 +2,16 @@ package ai.cyberlabs.yoonit.yoonit.handshake
 
 import android.content.Context
 import android.util.Base64
-import android.util.Log
 import com.wultra.android.sslpinning.*
 import com.wultra.android.sslpinning.integration.DefaultUpdateObserver
 import com.wultra.android.sslpinning.integration.powerauth.powerAuthCertStore
 import java.net.URL
 
-
-class Handshake(context: Context, handshakeListener: HandshakeListener) {
+class Handshake(context: Context) {
 
     private val myContext = context
 
-    private val listener = handshakeListener
-
-    fun updateFingerprint( publicKey: String, serviceUrl: String) {
+    fun updateFingerprint(publicKey: String, serviceUrl: String, result: (String) -> Unit) {
         val publicKeyByteArray: ByteArray = Base64.decode(publicKey, Base64.NO_WRAP)
 
         val configuration = CertStoreConfiguration.Builder(
@@ -28,16 +24,12 @@ class Handshake(context: Context, handshakeListener: HandshakeListener) {
         certStore.update(UpdateMode.DEFAULT, object : DefaultUpdateObserver() {
             override fun continueExecution() {
                 // Certstore is likely up-to-date, you can resume execution of your code.
-                listener.continueExecution()
+                result(UpdateResult.OK.name)
             }
 
             override fun handleFailedUpdate(type: UpdateType, result: UpdateResult) {
                 // There was an error during the update, present an error to the user.
-                if (result == UpdateResult.OK) {
-                    listener.continueExecution()
-                    return
-                }
-                listener.handleFailedUpdate(type.name, type.isPerformingUpdate, result.name)
+                result(result.name)
             }
         })
     }
